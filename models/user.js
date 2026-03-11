68a18e57fa4d1a1bcfe825c44ea66adb036d05ee
Harshiii 💫✨ <chauhanharshh@proton.me>
2026-03-11 18:59:45 +0530

    Update MongoDB connection string to use Atlas

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
+mongoose.connect("mongodb+srv://error:error@hotel-management-system.vvigqvp.mongodb.net/express_crud");
 
 
 const userSchema = mongoose.Schema({
